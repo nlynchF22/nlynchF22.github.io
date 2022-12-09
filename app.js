975d210f33ac6522dac20544ff81496e1dc633ce
Noah Lynch <120128738+nlynchF22@users.noreply.github.com>
2022-12-09 18:01:47 +0000

    Added colors to the page and also added the comments to the js code

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Defining the different elements that are needed
+//Defining the different elements that are needed for the program to work
 const btn = document.getElementById("btn")
 const drp_1 = document.getElementById("class")
 const drp_2 = document.getElementById("range")
@@ -63,30 +63,39 @@ const table = {
 
 
 
-drp_2.options[drp_2.selectedIndex].Innerhtml
 //Code for when the 'submit all' button is clicked
 btn.addEventListener("click", function Submit() {
+  // Clears all links for Frames after the sumbit button is clicked
   display.innerHTML = ''
+  // Gets information from the inputs of the dropdowns
   let drp1 = drp_1.options[drp_1.selectedIndex].innerHTML
   let drp2 = drp_2.options[drp_2.selectedIndex].innerHTML
+  // makes first filtered list
   let filteredFrames = []  
+  // Filters for the range of the Frames
   for(frames in table.frames){
     if (table.frames[frames].range == drp2 || drp2 == ""){
       filteredFrames.push(table.frames[frames])
     }
   }
+  // Makes the second filtered list
   let filteredFrames2 = []
+  // Filters for the class of the Frames
   for(let i = 0; filteredFrames.length>i; i++){
     if (filteredFrames[i].type == drp1 || drp1 == ""){
       filteredFrames2.push(filteredFrames[i])
     }
   }
+  // Gets url for tagged Frames
   for(let i = 0; filteredFrames2.length>i; i++){
     let url = document.createElement('a')
     url.href = filteredFrames2[i].url
     display.appendChild(url)
-    
-url.appendChild(document.createTextNode(filteredFrames2[i].url));    
-    display.appendChild(document.createElementNS("http://www.w3.org/1999/xhtml", "br"));
+
+// Displays the links for the Frames
+url.appendChild(document.createTextNode(filteredFrames2[i].url));
+// Puts breaks between each links so they are on seperate lines
+display.appendChild(document.createElementNS("http://www.w3.org/1999/xhtml", "br"));
   }
 });
+
